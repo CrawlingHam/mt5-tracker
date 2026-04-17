c652c048f6889ac6066b14424e42406a9c4ae9df
CrawlingHam <lnhopelessdays@gmail.com>
2026-04-17 17:41:05 +0200

    Fixed script port dependency

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.configs import IS_PRODUCTION, PORT, allowed_origins
 from src.middleware import setup_request_logger
 from src.routes import register_routes
 from flask_cors import CORS
+from waitress import serve
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,4 +11,7 @@ setup_request_logger(app)
 register_routes(app)
 
 if __name__ == "__main__":
-    app.run(port=PORT, debug=not IS_PRODUCTION)
+    if IS_PRODUCTION:
+        serve(app, listen=f"127.0.0.1:{PORT}")
+    else:
+        app.run(port=PORT, debug=True)
